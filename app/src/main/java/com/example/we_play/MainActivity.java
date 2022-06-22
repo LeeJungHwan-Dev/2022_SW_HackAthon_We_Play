74788a2e20f89e5327eb4170899d84269b3bc519
LeeJungHwan-Dev <aftermoa@gmail.com>
2022-06-22 18:20:18 +0900

    레이아웃 전체화면으로 변경 처음 시작 메인 액티비티 1.5초후 넘어가는 기능 구현

--- app/src/main/java/com/example/we_play/MainActivity.java
+++ app/src/main/java/com/example/we_play/MainActivity.java
@@ -2,14 +2,47 @@ package com.example.we_play;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 
+import java.util.Timer;
+import java.util.TimerTask;
+
 public class MainActivity extends AppCompatActivity {
+
+    Context context = this;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        /**
+         *
+         * 메인 시작 페이지로 첫 로딩 화면 입니다.
+         * 1초간의 대기를 하고 다음 페이지로 넘어갑니다.
+         * 차후 이 페이지에 계정 로그인 / 회원가입을 구현해야 합니다.
+         *
+         * */
+
+        Go_main_page(context);
+
+
+
+    }
+
+
+    public void Go_main_page(Context context){
+        TimerTask timerTask = new TimerTask() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(context,Main_page.class);
+                startActivity(intent);
+            }
+        };
+
+        Timer timer = new Timer();
+        timer.schedule(timerTask,1500);
     }
 }
