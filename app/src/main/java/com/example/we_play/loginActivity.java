16ad25a36df468d126e00761eeb9db0ee8cbe542
Sueun Kim <sueun024@gmail.com>
2022-06-23 21:36:30 +0900

    로그인 버튼 이미지 변경

--- app/src/main/java/com/example/we_play/loginActivity.java
+++ app/src/main/java/com/example/we_play/loginActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+import android.widget.ImageButton;
 import android.widget.Toast;
 import com.example.we_play.Module.RequestApiTask;
 import com.google.android.gms.auth.api.signin.GoogleSignIn;
