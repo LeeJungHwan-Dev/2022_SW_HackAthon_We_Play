9e74df3cd48d9913f381960c3a090e9b0ef91735
Sueun Kim <sueun024@gmail.com>
2022-06-24 06:59:01 +0900

    내정보 탈퇴 구현

--- app/src/main/java/com/example/we_play/info_delete_Activity.java
+++ app/src/main/java/com/example/we_play/info_delete_Activity.java
@@ -10,5 +10,6 @@ public class info_delete_Activity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_info_delete);
+
     }
 }
